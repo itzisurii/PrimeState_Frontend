32b2919d2b6bd0fdd55037addb9d7c8590c763ef
itzisurii <itzIsurika08@gmail.com>
2026-02-24 21:43:41 +0530

    Add PrimeLocation component with HTML, CSS, and test files

---
+++ src/app/components/prime-location/prime-location.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prime-location',
+  imports: [],
+  templateUrl: './prime-location.html',
+  styleUrl: './prime-location.css',
+})
+export class PrimeLocation {
+
+}
