1a473fed588cba047acaef20590df4444f5e2e87
itzisurii <itzIsurika08@gmail.com>
2026-02-25 17:08:29 +0530

    Update routing to use Dashboard component as default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Home
+        component: Dashboard
     }
 ];
