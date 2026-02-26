83b228749a778519b80174bb72a6fd3fcc87e37b
itzisurii <itzIsurika08@gmail.com>
2026-02-26 22:25:09 +0530

    Add ListingsHeader component with HTML, CSS, and test files

---
+++ src/app/layout/headers/listings-header/listings-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listings-header',
+  imports: [],
+  templateUrl: './listings-header.html',
+  styleUrl: './listings-header.css',
+})
+export class ListingsHeader {
+
+}
