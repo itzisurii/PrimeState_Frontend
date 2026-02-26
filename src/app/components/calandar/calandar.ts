787bda9e2970b25aa326c02cbc86d9f90edd982a
itzisurii <itzIsurika08@gmail.com>
2026-02-26 22:21:13 +0530

    Add Calandar component with HTML, CSS, and test files

---
+++ src/app/components/calandar/calandar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calandar',
+  imports: [],
+  templateUrl: './calandar.html',
+  styleUrl: './calandar.css',
+})
+export class Calandar {
+
+}
