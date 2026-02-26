3d615054c1ab3540a92f3a1ac4a53eb38e461d53
itzisurii <itzIsurika08@gmail.com>
2026-02-26 22:25:57 +0530

    Refactor dashboard layout by removing sidebar and header components

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { SideBar } from "../../components/side-bar/side-bar";
-import { Header } from "../../components/header/header";
 import { DashboardHero } from "../../components/dashboard-hero/dashboard-hero";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SideBar, Header, DashboardHero],
+  imports: [DashboardHero, SideBar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
