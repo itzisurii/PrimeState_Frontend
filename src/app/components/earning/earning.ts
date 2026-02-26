48a600f97215667bea3e1845ddc3ae861a638485
itzisurii <itzIsurika08@gmail.com>
2026-02-26 22:21:43 +0530

    Add Earning component with HTML, CSS, and test files

---
+++ src/app/components/earning/earning.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-earning',
+  imports: [],
+  templateUrl: './earning.html',
+  styleUrl: './earning.css',
+})
+export class Earning {
+
+}
