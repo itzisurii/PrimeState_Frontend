517f51f1d8ccf8460c32d86331c62cf758c83718
itzisurii <itzIsurika08@gmail.com>
2026-02-26 22:22:47 +0530

    Add MapSideBar component with HTML, CSS, and test files

---
+++ src/app/components/map-side-bar/map-side-bar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-side-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-side-bar.html',
+  styleUrls: ['./map-side-bar.css'],
+})
+export class MapSideBar {
+
+}
