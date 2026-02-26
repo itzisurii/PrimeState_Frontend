27c4047d398ed5c679ccb85c776d24d8105afec3
itzisurii <itzIsurika08@gmail.com>
2026-02-26 13:02:47 +0530

    Update routing to use Home component instead of Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Chatbot } from './components/chatbot/chatbot';
 
 export const routes: Routes = [
     {
         path: '',
-        component: Dashboard
+        component: Home
     }
 ];
